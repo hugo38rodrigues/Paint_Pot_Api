788f0d83086ddd31cdcbaabfc41193472d09c740
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-09-13 09:01:02 +0200

    format API rest

--- routes/paint-pots.routes.js
+++ routes/paint-pots.routes.js
@@ -5,13 +5,15 @@ export const router = Router()
 router.get('/', (req, res) => {
   res.json({ message: 'Voici mes données' })
 })
-router.post('/add', (req, res) => {
+
+router.post('/', (req, res) => {
   res.json({ message: req.body.message })
 })
 
-router.put('/modify/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   res.json({ messageId: req.params.id })
 })
-router.delete('/deleted/:id', (req, res) => {
+
+router.delete('/:id', (req, res) => {
   res.json({ message: `Post supprimé id ${req.params.id}` })
 })
